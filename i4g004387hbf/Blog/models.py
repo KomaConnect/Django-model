9a2a1493e3622e91cbb931677e10d55b876200ac
MustaphaKareem@KomaConnect <mustaphakareem16@gmail.com>
2022-06-17 10:55:27 +0100

    create an app 'Blog' with a model post in it.

---
+++ i4g004387hbf/Blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+user = get_user_model()
+
+#  to created my models 
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+
+
+    
